8219aaa225a3e5108f49448cc15016fcd73db23c
Lucas Caixeta <contato@lucascaixeta.com>
2017-02-11 00:39:30 +0000

    Reducers
    How to create a very simple reducer (and why)

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,13 @@
+/**
+ * Created by lucas on 11-02-2017.
+ */
+
+
+export default function() {
+	return [
+		{ title: 'Javascript: THe Good Parts' },
+		{ title: 'Harry Potter' },
+		{ title: 'The Dark Tower' },
+		{ title: 'Eloquent Ruby' },
+	]
+}
