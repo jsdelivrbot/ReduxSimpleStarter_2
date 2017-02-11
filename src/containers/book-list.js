b5897d6134ba11ee2d20ff5160417387729eebc2
Lucas Caixeta <contato@lucascaixeta.com>
2017-02-11 01:13:32 +0000

    Containers Continued and Implementations of a Container Class

--- src/containers/book-list.js
+++ src/containers/book-list.js
@@ -2,8 +2,9 @@
  * Created by lucas on 11-02-2017.
  */
 import React,{ Component } from 'react';
+import { connect } from 'react-redux';
 
-export default class BookList extends Component {
+class BookList extends Component {
 	
 	renderList() {
 		return this.props.books.map((book) => {
@@ -22,3 +23,13 @@ export default class BookList extends Component {
 		);
 	}
 }
+
+function mapStateToProps(state) {
+	// Whatever is returned will show up as props
+	// inside of BookList
+	return{
+		books: state.books
+	}
+}
+
+export default connect(mapStateToProps)(BookList);
