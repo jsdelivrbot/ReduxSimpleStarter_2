4f4ae1b3d41a4bce98e4356f2ed286703b4ad144
Lucas Caixeta <contato@lucascaixeta.com>
2017-04-13 18:10:22 +0100

    Conditional Rendering - Section 4, Lecture 49

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,11 +1,11 @@
 /**
  * Created by lucas on 11-02-2017.
  */
-export default function() {
-	return [
-		{ title: 'Javascript: THe Good Parts' },
-		{ title: 'Harry Potter' },
-		{ title: 'The Dark Tower' },
-		{ title: 'Eloquent Ruby' },
-	]
+export default function () {
+    return [
+        {title: 'Javascript: THe Good Parts', pages: 101},
+        {title: 'Harry Potter', pages: 39},
+        {title: 'The Dark Tower', pages: 85},
+        {title: 'Eloquent Ruby', pages: 1},
+    ]
 }
